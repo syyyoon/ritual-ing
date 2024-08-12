22c4ff9e6e4ef21903b939e7ae96a3e53889b7b1
SUNYOUNG YOON <hh24010016@gmail.com>
2024-08-12 15:55:40 +0900

    fix:캡처한 이미지파일  임시 저장소에서 영구 저장소에 복사하기

--- screens/ImagePickerScreen.tsx
+++ screens/ImagePickerScreen.tsx
@@ -1,10 +1,11 @@
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React, { useRef, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
+import * as FileSystem from "expo-file-system";
+import { captureRef } from "react-native-view-shot";
 import { useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamList } from "../type";
+import { ImagePickerScreenNavigation } from "../types/navigation";
 import IconButton from "../components/IconButton";
 import ImageViewer from "../components/ImageViewer";
 import Colors from "../constants/colors";
@@ -12,24 +13,18 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import EmojiPicker from "../components/EmojiPicker";
 import EmojiList from "../components/EmojiList";
 import DateEmoji from "../components/DateEmoji";
-import { captureRef } from "react-native-view-shot";
 
 const ImagePickerScreen = () => {
   const [imageUri, setImageUri] = useState<string | null>(null);
-  // action box (사진 앨범에서 사진 선택 시 열리는 action icons)
   const [showActionOptions, setShowActionOptions] = useState<boolean>(false);
-  // emoji modal
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-  // pick date emoji
   const [pickedEmoji, setPickedEmoji] = useState<string | null>(null);
-
   const [selectedColor, setSelectedColor] = useState<string>("#ffff");
 
   const imageRef = useRef<View>(null);
 
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList, "ImagePicker">>();
+  const navigation = useNavigation<ImagePickerScreenNavigation>();
 
-  // 이미지 앨범에서 pick
   const pickImage = async () => {
     const permissionMediaLibray = await MediaLibrary.requestPermissionsAsync();
 
@@ -54,7 +49,6 @@ const ImagePickerScreen = () => {
     }
   };
 
-  // 사진촬영
   const takePhoto = async () => {
     const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
 
@@ -73,8 +67,7 @@ const ImagePickerScreen = () => {
     }
   };
 
-  //이미지 내 라이브러리에 저장
-  const onSaveImageAsync = async () => {
+  const saveImageToLibrary = async () => {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -83,36 +76,41 @@ const ImagePickerScreen = () => {
 
       await MediaLibrary.saveToLibraryAsync(localUri);
       if (localUri) {
-        alert("Successfully Saved!");
+        alert("성공적으로 저장되었습니다!");
       }
     } catch (error) {
-      alert("Failed! Try again.");
+      alert("다시 시도해주세요.");
       console.log(error);
     }
   };
 
-  // 처음으로 돌아가기
   const onReset = () => {
     setShowActionOptions(false);
     setImageUri(null);
     setPickedEmoji(null);
   };
 
-  const handleColorSelect = (color: string) => {
+  const colorSelectHandler = (color: string) => {
     setSelectedColor(color);
-    // setIsModalVisible(false);
+    setIsModalVisible(false);
   };
 
-  //
   const captureAndNavigate = async () => {
     if (imageRef.current) {
       try {
-        const uri = await captureRef(imageRef.current, {
+        const tempUri = await captureRef(imageRef.current, {
           format: "png",
           quality: 1,
         });
-        console.log("uri", uri);
-        navigation.navigate("RitualForm", { imageUri: uri });
+
+        const newUri = `${FileSystem.documentDirectory}${Date.now()}.png`;
+
+        await FileSystem.copyAsync({
+          from: tempUri,
+          to: newUri,
+        });
+
+        navigation.navigate("RitualForm", { imageUri: newUri });
       } catch (error) {
         console.error("Error capturing view:", error);
       }
@@ -122,7 +120,7 @@ const ImagePickerScreen = () => {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View ref={imageRef} collapsable={false} style={{ position: "relative" }}>
-        <ImageViewer selectedImage={imageUri} />
+        <ImageViewer selectedImage={imageUri ?? undefined} />
         {pickedEmoji && (
           <DateEmoji
             text={pickedEmoji}
@@ -135,9 +133,7 @@ const ImagePickerScreen = () => {
 
       {showActionOptions ? (
         <View style={styles.buttonsContainer}>
-          {/* 새로고침 */}
           <IconButton iconType="MaterialIcons" icon="refresh" onPress={onReset} />
-          {/* 데이트 이모지 선택 */}
           <IconButton
             iconType="MaterialIcons"
             icon="auto-awesome"
@@ -145,15 +141,13 @@ const ImagePickerScreen = () => {
               setIsModalVisible(true);
             }}
           />
-          <IconButton iconType="MaterialIcons" icon="download" onPress={onSaveImageAsync} />
+          <IconButton iconType="MaterialIcons" icon="download" onPress={saveImageToLibrary} />
           <IconButton iconType="MaterialIcons" icon="check" onPress={captureAndNavigate} />
         </View>
       ) : (
         <View style={styles.buttonsContainer}>
           <IconButton iconType="MaterialIcons" icon="add-a-photo" onPress={takePhoto} />
           <IconButton iconType="MaterialIcons" icon="photo-library" onPress={pickImage} />
-
-          {/* skipt (사진 선택 x) */}
           <IconButton
             iconType="MaterialIcons"
             icon="skip-next"
@@ -165,12 +159,11 @@ const ImagePickerScreen = () => {
       )}
 
       <EmojiPicker
-        // selectedColor={selectedColor}
         isVisible={isModalVisible}
         onClose={() => {
           setIsModalVisible(false);
         }}
-        onColorSelected={handleColorSelect}
+        onColorSelected={colorSelectHandler}
       >
         <EmojiList onSelect={setPickedEmoji} pickedEmoji={pickedEmoji} selectedColor={selectedColor} />
       </EmojiPicker>

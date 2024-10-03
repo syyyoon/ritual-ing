680444274d8c4548f0b20c8246a85ccf82b29f89
SUNYOUNG YOON <hh24010016@gmail.com>
2024-10-03 16:45:18 +0900

    fix : 모바일 상에서 검색창에 focus 될 경우 키보드가 올라와 input에 입력값이 보이지 않는 문제 해결

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet, Text, ActivityIndicator, FlatList, TouchableOpacity } from "react-native";
+import { View, TextInput, StyleSheet, Text, ActivityIndicator, FlatList, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 import Colors from "../constants/colors";
 import { SearchScreenNavigationProp } from "../types/navigation";
 import CustomButton from "../components/CustomButton";
@@ -19,7 +19,7 @@ const SearchScreen = ({ navigation }: Props) => {
   const [searchText, setSearchText] = useState<string>("")
   const [filteredData, setFilteredData] = useState<RitualData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isSearched, setIsSearched] = useState<boolean>(false);  // 검색 여부 상태 추가
+  const [isSearched, setIsSearched] = useState<boolean>(false);
 
   const { theme } = useTheme();
   const closeModalhandler = () => {
@@ -28,7 +28,7 @@ const SearchScreen = ({ navigation }: Props) => {
 
 
   const searchHandler = async () => {
-    if (!searchText.trim()) {  // 입력값이 없을 경우 경고 메시지 표시
+    if (!searchText.trim()) {
       return;
     }
 
@@ -56,65 +56,69 @@ const SearchScreen = ({ navigation }: Props) => {
 
   useFocusEffect(
     React.useCallback(() => {
-      // 스크린에 다시 진입할 때 상태 초기화
       setIsLoading(false);
       setIsSearched(false);
-      // setErrorMessage("")
     }, [])
   );
 
 
   return (
-    <Layout>
-      {isLoading ? (  // 로딩 상태일 때 로딩중 메시지 표시
-        <View style={styles.indicatorWrapper}>
-          <ActivityIndicator size="large" color={Colors.PRIMARY} />
-          <Text style={{ color: theme.TEXT }}>조회중</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={90}
+    >
+      <Layout>
+        {isLoading ? (  // 로딩 상태일 때 로딩중 메시지 표시
+          <View style={styles.indicatorWrapper}>
+            <ActivityIndicator size="large" color={Colors.PRIMARY} />
+            <Text style={{ color: theme.TEXT }}>조회중</Text>
+          </View>
+        ) : (
+          <View>
+            {isSearched && filteredData.length === 0 ? (
+              // 검색 결과가 없을 때 표시할 메시지
+              <View style={{ alignItems: 'center', marginTop: 20 }}>
+                <Text style={{ color: theme.TEXT }}>검색한 단어에 대한 리추얼이 존재하지 않습니다.</Text>
+              </View>
+            ) : (
+              // 검색 결과가 있을 때 데이터를 보여줌
+              <View style={styles.listContainer}>
+                <FlatList
+                  data={filteredData}
+                  keyExtractor={(item) => item.id.toString()}
+                  numColumns={1}
+                  contentContainerStyle={{ paddingBottom: 180 }}
+                  showsVerticalScrollIndicator={true}
+                  renderItem={({ item }) => (
+                    <View style={styles.cardContainer} >
+                      <TouchableOpacity onPress={() => {
+                        closeModalhandler()
+                        navigation.navigate("Detail", { item })
+                      }}>
+                        <SearchResultCard item={item} />
+                      </TouchableOpacity>
+
+                    </View>
+                  )}
+                />
+              </View>
+            )}
+          </View>
+        )
+        }
+        <View style={[styles.modalContent, { backgroundColor: theme.DEFAULT_IMG_BG, }]}>
+          <TextInput placeholder="단어로 리추얼 로그를 찾아보세요 :)" placeholderTextColor={Colors.BORDER} style={[styles.input, { color: theme.TEXT }]} value={searchText} onChangeText={setSearchText} />
+          <FlexRowTexts
+            first={<CustomButton label="Search" theme="dark" onPress={searchHandler} />}
+            second={<CustomButton label="Close" theme="light" onPress={closeModalhandler} />}
+            style={{ marginTop: 10, justifyContent: "space-between" }}
+          />
+
         </View>
-      ) : (
-        <View>
-          {isSearched && filteredData.length === 0 ? (
-            // 검색 결과가 없을 때 표시할 메시지
-            <View style={{ alignItems: 'center', marginTop: 20 }}>
-              <Text style={{ color: theme.TEXT }}>검색한 단어에 대한 리추얼이 존재하지 않습니다.</Text>
-            </View>
-          ) : (
-            // 검색 결과가 있을 때 데이터를 보여줌
-            <View style={styles.listContainer}>
-              <FlatList
-                data={filteredData}
-                keyExtractor={(item) => item.id.toString()}
-                numColumns={1}
-                contentContainerStyle={{ paddingBottom: 180 }} // 마지막 카드가 잘리지 않도록 패딩 추가
-                showsVerticalScrollIndicator={true} // 스크롤바 숨김 (원하면 true로 설정)
-                renderItem={({ item }) => (
-                  <View style={styles.cardContainer} >
-                    <TouchableOpacity onPress={() => {
-                      closeModalhandler()
-                      navigation.navigate("Detail", { item })
-                    }}>
-                      <SearchResultCard item={item} />
-                    </TouchableOpacity>
 
-                  </View>
-                )}
-              />
-            </View>
-          )}
-        </View>
-      )
-      }
-      <View style={[styles.modalContent, { backgroundColor: theme.DEFAULT_IMG_BG, }]}>
-        <TextInput placeholder="단어로 리추얼 로그를 찾아보세요 :)" placeholderTextColor={Colors.BORDER} style={[styles.input, { color: theme.TEXT }]} value={searchText} onChangeText={setSearchText} />
-        <FlexRowTexts
-          first={<CustomButton label="Search" theme="dark" onPress={searchHandler} />}
-          second={<CustomButton label="Close" theme="light" onPress={closeModalhandler} />}
-          style={{ marginTop: 10, justifyContent: "space-between" }}
-        />
-
-      </View>
-
-    </Layout>
+      </Layout>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -153,7 +157,6 @@ const styles = StyleSheet.create({
   },
   cardContainer: {
     width: "100%",
-    // borderWidth: 1,
     marginBottom: 10,
   }
 });
